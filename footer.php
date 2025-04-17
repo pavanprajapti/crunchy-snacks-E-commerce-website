4d9e96dbab8791451b844cb5adf6d1e743c00569
pavanprajapti <163954368+pavanprajapti@users.noreply.github.com>
2025-04-17 12:56:41 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,82 @@
+<!-- footer -->
+
+<footer class="mt-5 py-5">
+    <div class="row container mx-auto pt-5">
+        <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+            
+            <h4 class="footer">About Khakhra Delight</h4>
+            <p class="pt-3">
+                We offer a variety of authentic,
+                healthy, and delicious Khakhra flavors
+                that represent the taste of tradition.
+                Made with premium ingredients,
+                every bite brings you the
+                richness of Indian snacks.</p>
+        </div>
+
+        <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+            <h5 class="pb-2">featured</h5>
+            <ul class="text-uppercase">
+                <li><a href="#">regular khakhra</a></li>
+                <li><a href="#">flavor khakhra</a></li>
+
+
+            </ul>
+        </div>
+
+        <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+            <h5 class="pb-2">contact Us</h5>
+            <div>
+                <h6 class="text-uppercase">Address</h6>
+                <p>1234 Street Name, city</p>
+            </div>
+
+            <div>
+                <h6 class="text-uppercase">Phone</h6>
+                <p>901 623 5766</p>
+            </div>
+
+            <div>
+                <h6 class="text-uppercase">E-mail</h6>
+                <p>jaiminbhatiya@g-mail.com</p>
+            </div>
+        </div>
+        <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+            <h5 class="pb-2">Instagram</h5>
+            <div class="row">
+                <img src="assets/imges/Regular.jpeg" class="img-fluid w-25 h-100 m-2" />
+                <img src="assets/imges/futures4.jpeg" class="img-fluid w-25 h-100 m-2" />
+                <img src="assets/imges/futures2.jpeg" class="img-fluid w-25 h-100 m-2" />
+                <img src="assets/imges/Regular4.jpeg" class="img-fluid w-25 h-100 m-2" />
+                <img src="assets/imges/Regular3.jpeg" class="img-fluid w-25 h-100 m-2" />
+            </div>
+        </div>
+    </div>
+
+    <div class="copyright mt-5">
+        <div class="row container mx-auto">
+            <div class="col-lg-3 col-md-5 col-sm-12 mb-4">
+                <img src="assets/imges/payment.jpeg" />
+            </div>
+
+            <div class="col-lg-3 col-md-5 col-sm-12 mb-4 text-nowrap mb-2">
+                <p>ecommerce @ 2025 All right Reserved</p>
+            </div>
+
+            <div class="col-lg-3 col-md-5 col-sm-12 mb-4">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
